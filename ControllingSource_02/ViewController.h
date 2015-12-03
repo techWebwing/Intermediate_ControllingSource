3dd1057f9cc09b0e4062f86ada663962253a5030
abdul karim <abdul.karim002@gmail.com>
2015-12-03 14:47:49 +0530

    Initial Commit

---
+++ ControllingSource_02/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ControllingSource_02
+//
+//  Created by Rohan Pawar on 03/12/15.
+//  Copyright © 2015 webwing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ControllingSource_02Tests/ControllingSource_02Tests.m
@@ -0,0 +1,39 @@
+//
+//  ControllingSource_02Tests.m
+//  ControllingSource_02Tests
+//
+//  Created by Rohan Pawar on 03/12/15.
+//  Copyright © 2015 webwing. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ControllingSource_02Tests : XCTestCase
+
+@end
+
+@implementation ControllingSource_02Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
